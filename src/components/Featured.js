a35a67cc00b6f0b17a452baec1b2f55f6701ca9e
gsaurabh2311 <53432550+gsaurabh2311@users.noreply.github.com>
2022-02-21 19:18:25 +0530

    Commit

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import BTC from '../assets/btc-img.png'
+import React, {useState, useEffect} from 'react'
+// import BTC from '../assets/btc-img.png'
 import {FiArrowUpRight, FiArrowDown} from 'react-icons/fi'
 import './Featured.css'
+import axios from 'axios'
 
 const Featured = () => {
-  return (
+    const [data, setData] = useState(null)
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false'
+    useEffect(() => {
+        axios.get(url).then((response) => {
+                setData(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, [])
+    // console.log(data)
+
+    if(!data) return null
+
+    return (
     <div className="featured">
         <div className="container">
             <div className="left">
@@ -14,14 +28,43 @@ const Featured = () => {
             </div>
 
             <div className="right">
-                <div className="top">
-                    <img src={BTC} alt="" />
-                </div>
-                <div>
-                    <h5>Bitcoin</h5>
-                    <p>$50000</p>
-                </div>
-                <span><FiArrowUpRight />2.5%</span>
+            {data.map((card) => (
+                                    <div className='card'>
+                                          <div className='top'>
+                                                <img src={card.image} alt='/' />
+                                          </div>
+                                          <div>
+                                                <h5>{card.name}</h5>
+                                                <p>${card.current_price.toLocaleString()}</p>
+                                          </div>
+                                          {card.price_change_percentage_24h < 0 ? (
+                                                <span className='red'>
+                                                      <FiArrowDown className='icon'/>
+                                                      {card.price_change_percentage_24h.toFixed(2)}%
+                                                </span>
+                                          ) : (
+                                                <span className='green'>
+                                                      <FiArrowUpRight className='icon'/>
+                                                      {card.price_change_percentage_24h.toFixed(2)}%
+                                                </span>
+                                          )}
+                                    </div>
+                              ))}
+                {/* <div className="card">
+                    <div className="top">
+                        <img src={data[0].image} alt="" />
+                    </div>
+                    <div>
+                        <h5>{data[0].name}</h5>
+                        <p>${data[0].current_price.toLocaleString()}</p>
+                    </div>
+
+                    {data[0].price_change_percentage_24h < 0 ? (
+                    <span className='red'><FiArrowDown />{data[0].price_change_percentage_24h.toFixed(2)}%</span>
+                    ) : (
+                    <span className='green'><FiArrowUpRight />{data[0].price_change_percentage_24h.toFixed(2)}%</span>
+                    )}
+                </div>     */}
             </div>
         </div>
     </div>
