b4dba35b0f0d27531b3a5200509570295ed5a27e
gsaurabh2311 <53432550+gsaurabh2311@users.noreply.github.com>
2022-02-22 01:46:20 +0530

    Footer Initial Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+import {FaFacebook, FaLinkedin, FaTwitter, FaGithub} from 'react-icons/fa'
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
