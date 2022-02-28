728358a9ca034fbcd4f556d056afcf0501d000c1
gsaurabh2311 <53432550+gsaurabh2311@users.noreply.github.com>
2022-02-28 14:12:46 +0530

    Final Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,32 +12,32 @@ const Footer = () => {
             <div className="col">
                 <h5>Support</h5>
                 <span className="bar"></span>
-                    <a href='#'>Contact Us</a>
-                    <a href='#'>Chat</a>
-                    <a href='#'>Help Center</a>
-                    <a href='#'>Developers</a>
+                    <a href='/#'>Contact Us</a>
+                    <a href='/#'>Chat</a>
+                    <a href='/#'>Help Center</a>
+                    <a href='/#'>Developers</a>
             </div>
             <div className="col">        
                 <h5>Developers</h5>
                 <span className="bar"></span>
-                    <a href='#'>Cloud</a>
-                    <a href='#'>Api</a>
-                    <a href='#'>Pro</a>
+                    <a href='/#'>Cloud</a>
+                    <a href='/#'>Api</a>
+                    <a href='/#'>Pro</a>
             </div>
             <div className="col">    
                 <h5>Company</h5>
                 <span className="bar"></span>
-                    <a href='#'>About</a>
-                    <a href='#'>Privacy</a>
-                    <a href='#'>Legal</a>
+                    <a href='/#'>About</a>
+                    <a href='/#'>Privacy</a>
+                    <a href='/#'>Legal</a>
             </div>
             <div className="col">    
                 <h5>Social</h5>
                 <span className="bar"> </span>
-                    <a href='#'><FaFacebook className="icon" /></a>
-                    <a href='#'><FaTwitter className="icon" /></a>
-                    <a href='#'><FaLinkedin className="icon" /></a>
-                    <a href='#'><FaGithub className="icon" /></a>
+                    <a href='/#'><FaFacebook className="icon" /></a>
+                    <a href='/#'><FaTwitter className="icon" /></a>
+                    <a href='/#'><FaLinkedin className="icon" /></a>
+                    <a href='/#'><FaGithub className="icon" /></a>
             </div>
         </div>    
     </div>
